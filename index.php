ff83e213f73fdf11b699ec5d2b9f00db983faa73
WuGaoZan <75108805+WuGaoZan@users.noreply.github.com>
2020-11-27 14:55:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,45 @@
 <?php
-  $link=new mysqli('192.168.0.100', 'user', '548794877414', 'weight');
-  $select="SELECT * FROM test WHERE 1";
-  $search=$link->query($select);
-  echo '<table border=0 width=250>';
-  echo '<tr>';
-  echo '<td>tid</td>';
-  echo '<td>nid</td>';
-  echo '<td>weight</td>';
-  echo '</tr>';
-  while($data=$search->fetch_assoc())
-  {
-	  echo '<tr>';
-	  echo '<td>'.$data['tid'].'</td>';
-	  echo '<td>'.$data['nid'].'</td>';
-	  echo '<td>'.$data['weight'].'</td>';
-	  echo '</tr>';
-  }
-  echo '</table>';
-  $select="SELECT SUM(weight) AS SUM FROM test WHERE 1";
-  $search=$link->query($select);
-  $data=$search->fetch_assoc();
-  echo '</br></br>SUM(weight)='.$data['SUM'];
-  $link->close();
+	function getLocalIP()
+	{
+		$preg = "/\A((([0-9]?[0-9])|(1[0-9]{2})|(2[0-4][0-9])|(25[0-5]))\.){3}(([0-9]?[0-9])|(1[0-9]{2})|(2[0-4][0-9])|(25[0-5]))\Z/";
+		//獲取作業系統為win2000/xp、win7的本機IP真實地址
+		exec("ipconfig", $out, $stats);
+		if (!empty($out))
+		{
+			foreach ($out AS $row)
+			{
+				if (strstr($row, "IP") && strstr($row, ":") && !strstr($row, "IPv6"))
+				{
+					$tmpIp = explode(":", $row);
+					if (preg_match($preg, trim($tmpIp[1])))
+					{
+						return trim($tmpIp[1]);
+					}
+				}
+			}
+		}
+	}
+	$link=new mysqli(getLocalIP(), 'user', '548794877414', 'weight');
+	$select="SELECT * FROM test WHERE 1";
+	$search=$link->query($select);
+	echo '<table border=0 width=250>';
+	echo '<tr>';
+	echo '<td>tid</td>';
+	echo '<td>nid</td>';
+	echo '<td>weight</td>';
+	echo '</tr>';
+	while($data=$search->fetch_assoc())
+	{
+		echo '<tr>';
+		echo '<td>'.$data['tid'].'</td>';
+		echo '<td>'.$data['nid'].'</td>';
+		echo '<td>'.$data['weight'].'</td>';
+		echo '</tr>';
+	}
+	echo '</table>';
+	$select="SELECT SUM(weight) AS SUM FROM test WHERE 1";
+	$search=$link->query($select);
+	$data=$search->fetch_assoc();
+	echo '</br></br>SUM(weight)='.$data['SUM'];
+	$link->close();
 ?>
