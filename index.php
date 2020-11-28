e37e67bd4dac0408c151da9f48620adbd044f1c1
WuGaoZan <75108805+WuGaoZan@users.noreply.github.com>
2020-11-28 13:34:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
 <?php
-//$nid=$_GET['id'];
-//$weight=$_GET['w'];
-$link=new mysqli('192.168.0.100', 'user', '548794877414', 'weight');
-if ($link -> connect_errno)
-{
-  echo "Failed to connect to MySQL: ". $link -> connect_errno . $link -> connect_error;
+$nid=$_GET['id'];
+$weight=$_GET['w'];
+$link=new mysqli('192.168.0.100', 'user', 'tachitsuteto123', 'weight');
+if ($link -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $link -> connect_error;
   exit();
 }
-//$sql="INSERT INTO `test`(`nid`, `weight`) VALUES ($nid,$weight)";
-//$addata=$link->query($sql);
-$select="SELECT * FROM test WHERE 1";
+$sql="INSERT INTO `weight`(`nid`, `weight`) VALUES ($nid,$weight)";
+$addata=$link->query($sql);
+$select="SELECT * FROM weight WHERE 1";
 $search=$link->query($select);
 echo '<table border=0 width=250>';
 echo '<tr>';
-echo '<td>tid</td>';
+echo '<td>wid</td>';
 echo '<td>nid</td>';
 echo '<td>weight</td>';
 echo '</tr>';
 while($data=$search->fetch_assoc())
 {
 	echo '<tr>';
-	echo '<td>'.$data['tid'].'</td>';
+	echo '<td>'.$data['wid'].'</td>';
 	echo '<td>'.$data['nid'].'</td>';
 	echo '<td>'.$data['weight'].'</td>';
 	echo '</tr>';
 }
 echo '</table>';
-$select="SELECT SUM(weight) AS SUM FROM test WHERE 1";
+$select="SELECT SUM(weight) AS SUM FROM weight WHERE 1";
 $search=$link->query($select);
 $data=$search->fetch_assoc();
 echo '</br></br>SUM(weight)='.$data['SUM'];
