e92a413237e1f26377ec77025872f92bd1536348
WuGaoZan <75108805+WuGaoZan@users.noreply.github.com>
2020-12-08 20:44:05 +0800

    Create data.php

---
+++ data.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<form action='index.html'>
+<input type='submit' value='back'/>
+</form>
+</body>
+</html>
+<?php
+$ip='localhost';
+$user='prj_10905';
+$pw='FDqWDXA12XIY7Zit';
+$db='prj_10905';
+$link=new mysqli($ip, $user, $pw, $db);
+if ($link -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $link -> connect_error;
+  exit();
+}
+$sel="SELECT * FROM data WHERE 1";
+$sch=$link->query($sel);
+echo '<table border=0 width=250>';
+echo '<tr>';
+echo '<td>wid</td>';
+echo '<td>weight</td>';
+echo '</tr>';
+while($data=$sch->fetch_assoc())
+{
+	echo '<tr>';
+	echo '<td>'.$data['wid'].'</td>';
+	echo '<td>'.$data['weight'].'</td>';
+	echo '</tr>';
+}
+echo '</table>';
+$link->close();
+?>
